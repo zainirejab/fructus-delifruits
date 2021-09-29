e261f9387a44fbf48531adab3140f1607915e5ec
zainirejab <zainirejab@gmail.com>
2021-09-29 17:02:30 +0300

    First commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,23 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Bismillah on 24.9.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
+    var body: some Scene {
+        WindowGroup {
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
